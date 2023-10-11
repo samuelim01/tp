6b41d97714e473d959d26447b008ba1d5decced7
samuelim01 <samuelim01@gmail.com>
2023-10-12 00:01:30 +0800

    ModelManager: Add tests for set and delete methods
    Add tests for setPerson() and deletePerson().
    
    Add similar tests for Guest and Vendor.

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -3,12 +3,16 @@ package seedu.address.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalGuests.GEORGE;
+import static seedu.address.testutil.TypicalGuests.GREG;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.BOB;
 import static seedu.address.testutil.TypicalVendors.ANNE;
+import static seedu.address.testutil.TypicalVendors.BRYAN;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,8 +21,20 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.model.person.Guest;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Vendor;
+import seedu.address.model.person.exceptions.DuplicateGuestException;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.DuplicateVendorException;
+import seedu.address.model.person.exceptions.GuestNotFoundException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.person.exceptions.VendorNotFoundException;
 import seedu.address.testutil.AddressBookBuilder;
+import seedu.address.testutil.GuestBuilder;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.VendorBuilder;
 
 public class ModelManagerTest {
 
@@ -91,6 +107,74 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void deletePerson_nullPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.deletePerson(null));
+    }
+
+    @Test
+    public void deletePerson_personDoesNotExist_throwsPersonNotFoundException() {
+        assertThrows(PersonNotFoundException.class, () -> modelManager.deletePerson(ALICE));
+    }
+
+    @Test
+    public void deletePerson_existingPerson_deletesPerson() {
+        modelManager.addPerson(ALICE);
+        modelManager.deletePerson(ALICE);
+        ModelManager expectedModelManager = new ModelManager();
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setPerson_nullTargetPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setPerson(null, ALICE));
+    }
+
+    @Test
+    public void setPerson_nullEditedPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setPerson(ALICE, null));
+    }
+
+    @Test
+    public void setPerson_targetPersonDoesNotExist_throwsNullPointerException() {
+        assertThrows(PersonNotFoundException.class, () -> modelManager.setPerson(ALICE, ALICE));
+    }
+
+    @Test
+    public void setPerson_editedPersonIsSamePerson_success() {
+        modelManager.addPerson(ALICE);
+        modelManager.setPerson(ALICE, ALICE);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addPerson(ALICE);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setPerson_editedPersonHasSameIdentity_success() {
+        modelManager.addPerson(ALICE);
+        Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).build();
+        modelManager.setPerson(ALICE, editedAlice);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addPerson(editedAlice);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setPerson_editedPersonHasDifferentIdentity_success() {
+        modelManager.addPerson(ALICE);
+        modelManager.setPerson(ALICE, BOB);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addPerson(BOB);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setPerson_editedPersonIsNonUnique_throwsDuplicatePersonException() {
+        modelManager.addPerson(ALICE);
+        modelManager.addPerson(BOB);
+        assertThrows(DuplicatePersonException.class, () -> modelManager.setPerson(ALICE, BOB));
+    }
+
+    @Test
     public void hasGuest_nullGuest_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> modelManager.hasGuest(null));
     }
@@ -107,6 +191,74 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void deleteGuest_nullGuest_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.deleteGuest(null));
+    }
+
+    @Test
+    public void deleteGuest_vendorDoesNotExist_throwsGuestNotFoundException() {
+        assertThrows(GuestNotFoundException.class, () -> modelManager.deleteGuest(GEORGE));
+    }
+
+    @Test
+    public void deleteGuest_existingGuest_deletesGuest() {
+        modelManager.addGuest(GEORGE);
+        modelManager.deleteGuest(GEORGE);
+        ModelManager expectedModelManager = new ModelManager();
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setGuest_nullTargetGuest_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setGuest(null, GEORGE));
+    }
+
+    @Test
+    public void setGuest_nullEditedGuest_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setGuest(GEORGE, null));
+    }
+
+    @Test
+    public void setGuest_targetGuestDoesNotExist_throwsNullPointerException() {
+        assertThrows(GuestNotFoundException.class, () -> modelManager.setGuest(GEORGE, GEORGE));
+    }
+
+    @Test
+    public void setGuest_editedGuestIsSameGuest_success() {
+        modelManager.addGuest(GEORGE);
+        modelManager.setGuest(GEORGE, GEORGE);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addGuest(GEORGE);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setGuest_editedGuestHasSameIdentity_success() {
+        modelManager.addGuest(GEORGE);
+        Guest editedGeorge = new GuestBuilder(GEORGE).withAddress(VALID_ADDRESS_BOB).build();
+        modelManager.setGuest(GEORGE, editedGeorge);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addGuest(editedGeorge);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setGuest_editedGuestHasDifferentIdentity_success() {
+        modelManager.addGuest(GEORGE);
+        modelManager.setGuest(GEORGE, GREG);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addGuest(GREG);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setGuest_editedGuestIsNonUnique_throwsDuplicateGuestException() {
+        modelManager.addGuest(GEORGE);
+        modelManager.addGuest(GREG);
+        assertThrows(DuplicateGuestException.class, () -> modelManager.setGuest(GEORGE, GREG));
+    }
+
+    @Test
     public void hasVendor_nullVendor_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> modelManager.hasVendor(null));
     }
@@ -120,6 +272,74 @@ public class ModelManagerTest {
     public void hasVendor_vendorInAddressBook_returnsTrue() {
         modelManager.addVendor(ANNE);
         assertTrue(modelManager.hasVendor(ANNE));
+    }
+
+    @Test
+    public void deleteVendor_nullVendor_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.deleteVendor(null));
+    }
+
+    @Test
+    public void deleteVendor_vendorDoesNotExist_throwsVendorNotFoundException() {
+        assertThrows(VendorNotFoundException.class, () -> modelManager.deleteVendor(ANNE));
+    }
+
+    @Test
+    public void deleteVendor_existingVendor_deletesVendor() {
+        modelManager.addVendor(ANNE);
+        modelManager.deleteVendor(ANNE);
+        ModelManager expectedModelManager = new ModelManager();
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setVendor_nullTargetVendor_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setVendor(null, ANNE));
+    }
+
+    @Test
+    public void setVendor_nullEditedVendor_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setVendor(ANNE, null));
+    }
+
+    @Test
+    public void setVendor_targetVendorDoesNotExist_throwsNullPointerException() {
+        assertThrows(VendorNotFoundException.class, () -> modelManager.setVendor(ANNE, ANNE));
+    }
+
+    @Test
+    public void setVendor_editedVendorIsSameVendor_success() {
+        modelManager.addVendor(ANNE);
+        modelManager.setVendor(ANNE, ANNE);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addVendor(ANNE);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setVendor_editedVendorHasSameIdentity_success() {
+        modelManager.addVendor(ANNE);
+        Vendor editedAnne = new VendorBuilder(ANNE).withAddress(VALID_ADDRESS_BOB).build();
+        modelManager.setVendor(ANNE, editedAnne);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addVendor(editedAnne);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setVendor_editedVendorHasDifferentIdentity_success() {
+        modelManager.addVendor(ANNE);
+        modelManager.setVendor(ANNE, BRYAN);
+        ModelManager expectedModelManager = new ModelManager();
+        expectedModelManager.addVendor(BRYAN);
+        assertEquals(modelManager, expectedModelManager);
+    }
+
+    @Test
+    public void setVendor_editedVendorIsNonUnique_throwsDuplicateVendorException() {
+        modelManager.addVendor(ANNE);
+        modelManager.addVendor(BRYAN);
+        assertThrows(DuplicateVendorException.class, () -> modelManager.setVendor(ANNE, BRYAN));
     }
 
     @Test
