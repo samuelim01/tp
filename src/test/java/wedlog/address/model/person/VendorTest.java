f5e7c948c732d7c5b6d2dbf286da426c8c008c3c
samuelim01 <samuelim01@gmail.com>
2023-10-16 17:46:53 +0800

    VendorTest: Add more equals tests

--- src/test/java/wedlog/address/model/person/VendorTest.java
+++ src/test/java/wedlog/address/model/person/VendorTest.java
@@ -4,12 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static wedlog.address.model.person.PersonTest.VALID_ADDRESS;
 import static wedlog.address.model.person.PersonTest.VALID_EMAIL;
 import static wedlog.address.model.person.PersonTest.VALID_NAME;
 import static wedlog.address.model.person.PersonTest.VALID_PHONE;
 import static wedlog.address.model.person.PersonTest.VALID_TAGS;
 import static wedlog.address.testutil.Assert.assertThrows;
+import static wedlog.address.testutil.TypicalVendors.ANNE;
+import static wedlog.address.testutil.TypicalVendors.BRYAN;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +24,7 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import wedlog.address.model.tag.Tag;
+import wedlog.address.testutil.VendorBuilder;
 
 /**
  * Simplified test class for Vendor.
@@ -93,28 +101,53 @@ public class VendorTest {
 
     @Test
     public void equals() {
-        Name name = new Name("ABC Catering");
-        Phone phone = new Phone("61234567");
-        Email email = new Email("abc@catering.com");
-        Address address = new Address("Blk 123");
-        Tag tag = new Tag("caterer");
-        Set<Tag> tags = new HashSet<>();
-        tags.add(tag);
-
-        Vendor vendor = new Vendor(name, phone, email, address, tags);
-
         // same values -> returns true
-        Person vendorCopy = new Vendor(name, phone, email, address, tags);
-        assertTrue(vendor.equals(vendorCopy));
+        Vendor anneCopy = new VendorBuilder(ANNE).build();
+        assertTrue(ANNE.equals(anneCopy));
 
         // same object -> returns true
-        assertTrue(vendor.equals(vendor));
+        assertTrue(ANNE.equals(ANNE));
 
         // null -> returns false
-        assertFalse(vendor.equals(null));
+        assertFalse(ANNE.equals(null));
 
         // different type -> returns false
-        assertFalse(vendor.equals(5));
+        assertFalse(ANNE.equals(5));
+
+        // different vendor -> returns false
+        assertFalse(ANNE.equals(BRYAN));
+
+        // different name -> returns false
+        Vendor editedAnne = new VendorBuilder(ANNE).withName(VALID_NAME_BOB).build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // different phone -> returns false
+        editedAnne = new VendorBuilder(ANNE).withPhone(VALID_PHONE_BOB).build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // different email -> returns false
+        editedAnne = new VendorBuilder(ANNE).withEmail(VALID_EMAIL_BOB).build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // different address -> returns false
+        editedAnne = new VendorBuilder(ANNE).withAddress(VALID_ADDRESS_BOB).build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // different tags -> returns false
+        editedAnne = new VendorBuilder(ANNE).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // no phone -> returns false
+        editedAnne = new VendorBuilder(ANNE).withoutPhone().build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // no email -> returns false
+        editedAnne = new VendorBuilder(ANNE).withoutEmail().build();
+        assertFalse(ANNE.equals(editedAnne));
+
+        // no address -> returns false
+        editedAnne = new VendorBuilder(ANNE).withoutAddress().build();
+        assertFalse(ANNE.equals(editedAnne));
     }
 
     @Test
