9352d2d573402a44565cd2ed3ae5333dfb63a515
wasjoe1 <joechua247@gmail.com>
2023-10-18 14:29:07 +0800

    minor nit

--- src/test/java/wedlog/address/logic/commands/CommandTestUtil.java
+++ src/test/java/wedlog/address/logic/commands/CommandTestUtil.java
@@ -154,12 +154,13 @@ public class CommandTestUtil {
         model.updateFilteredGuestList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
 
         assertEquals(1, model.getFilteredGuestList().size());
-    
+    }
+
     /**
      * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
      * {@code model}'s address book.
      */
-    public static void showVendorAtIndex(Model model, Index targetIndex) {
+    public static void showVendorAtIndex (Model model, Index targetIndex){
         assertTrue(targetIndex.getZeroBased() < model.getFilteredVendorList().size());
 
         Vendor vendor = model.getFilteredVendorList().get(targetIndex.getZeroBased());
