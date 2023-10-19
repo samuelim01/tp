e3ea89ba67c6df9eb0f9297976ee945e2d9bf7b7
samuelim01 <samuelim01@gmail.com>
2023-10-19 22:32:38 +0800

    VendorAddCommandParser: Add additional tests

--- src/test/java/wedlog/address/logic/parser/VendorAddCommandParserTest.java
+++ src/test/java/wedlog/address/logic/parser/VendorAddCommandParserTest.java
@@ -11,6 +11,7 @@ import static wedlog.address.logic.commands.CommandTestUtil.INVALID_ADDRESS_DESC
 import static wedlog.address.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
 import static wedlog.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static wedlog.address.logic.commands.CommandTestUtil.INVALID_PHONE_DESC;
+import static wedlog.address.logic.commands.CommandTestUtil.INVALID_TAG_DESC;
 import static wedlog.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static wedlog.address.logic.commands.CommandTestUtil.NAME_DESC_BOB;
 import static wedlog.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
@@ -20,6 +21,7 @@ import static wedlog.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
 import static wedlog.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
 import static wedlog.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static wedlog.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static wedlog.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static wedlog.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static wedlog.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static wedlog.address.testutil.TypicalVendors.AMY;
@@ -28,9 +30,12 @@ import org.junit.jupiter.api.Test;
 
 import wedlog.address.logic.commands.VendorAddCommand;
 import wedlog.address.logic.parser.exceptions.ParseException;
+import wedlog.address.model.person.Address;
+import wedlog.address.model.person.Email;
 import wedlog.address.model.person.Name;
 import wedlog.address.model.person.Phone;
 import wedlog.address.model.person.Vendor;
+import wedlog.address.model.tag.Tag;
 import wedlog.address.testutil.VendorBuilder;
 
 public class VendorAddCommandParserTest {
@@ -112,6 +117,17 @@ public class VendorAddCommandParserTest {
         assertParseFailure(parser, NAME_DESC_BOB + INVALID_PHONE_DESC + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
                 + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Phone.MESSAGE_CONSTRAINTS);
 
+        // invalid email
+        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + INVALID_EMAIL_DESC + ADDRESS_DESC_BOB
+                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Email.MESSAGE_CONSTRAINTS);
+
+        // invalid address
+        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
+                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Address.MESSAGE_CONSTRAINTS);
+
+        // invalid tag
+        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
+                + INVALID_TAG_DESC + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC,
