32a20b1912da81b573f07f231fa3f47ecd86cff6
Keagan <keaganpzh@gmail.com>
2023-10-19 20:30:47 +0800

    fix checkstyle

--- src/main/java/wedlog/address/ui/GuestCard.java
+++ src/main/java/wedlog/address/ui/GuestCard.java
@@ -25,10 +25,10 @@ public class GuestCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    public final Guest guest;
-
     private static final String RSVP_YES = "yes";
     private static final String RSVP_NO = "no";
+
+    public final Guest guest;
 
     @FXML
     private HBox cardPane;
