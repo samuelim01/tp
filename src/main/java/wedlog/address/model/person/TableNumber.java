1a64a487abb2d2a3f3191a2662ecce4c5b1954fa
tllshan <shantsou03@gmail.com>
2023-10-19 17:56:31 +0800

    Create table number class

---
+++ src/main/java/wedlog/address/model/person/TableNumber.java
@@ -0,0 +1,61 @@
+package wedlog.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static wedlog.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's table number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidTableNumber(String)}
+ */
+public class TableNumber {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Table numbers should only contain numbers";
+    public static final String VALIDATION_REGEX = "\\d+";
+    public final String value;
+
+    /**
+     * Constructs a {@code TableNumber}.
+     *
+     * @param tableNumber A valid table number.
+     */
+    public TableNumber(String tableNumber) {
+        requireNonNull(tableNumber);
+        checkArgument(isValidTableNumber(tableNumber), MESSAGE_CONSTRAINTS);
+        value = tableNumber;
+    }
+
+    /**
+     * Returns true if a given string is a valid table number.
+     */
+    public static boolean isValidTableNumber(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TableNumber)) {
+            return false;
+        }
+
+        TableNumber otherTableNumber = (TableNumber) other;
+        return value.equals(otherTableNumber.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
