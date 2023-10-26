9f1d139dcb7d4e989f301a2c8c71f5fd1e0669b8
tllshan <shantsou03@gmail.com>
2023-10-26 13:12:36 +0800

    Improve guest and vendor list visuals

--- src/main/java/wedlog/address/ui/VendorCard.java
+++ src/main/java/wedlog/address/ui/VendorCard.java
@@ -49,9 +49,14 @@ public class VendorCard extends UiPart<Region> {
         this.vendor = vendor;
         id.setText(displayedIndex + ". ");
         name.setText(vendor.getName().fullName);
-        phone.setText(vendor.getPhone().map(p -> p.value).orElse("-"));
-        address.setText(vendor.getAddress().map(a -> a.value).orElse("-"));
-        email.setText(vendor.getEmail().map(e -> e.value).orElse("-"));
+        phone.setText(vendor.getPhone().map(p -> p.value).orElse(""));
+        address.setText(vendor.getAddress().map(a -> a.value).orElse(""));
+        email.setText(vendor.getEmail().map(e -> e.value).orElse(""));
+
+        phone.setManaged(!phone.getText().isEmpty());
+        email.setManaged(!email.getText().isEmpty());
+        address.setManaged(!address.getText().isEmpty());
+
         vendor.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
