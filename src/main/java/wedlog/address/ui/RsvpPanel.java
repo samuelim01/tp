ae4cf86c1873e6802927dac211e3bb289d449b62
tllshan <shantsou03@gmail.com>
2023-10-27 23:39:40 +0800

    Style rsvp pie chart

--- src/main/java/wedlog/address/ui/RsvpPanel.java
+++ src/main/java/wedlog/address/ui/RsvpPanel.java
@@ -1,12 +1,16 @@
 package wedlog.address.ui;
 
+import static javafx.geometry.Side.LEFT;
+
 import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import wedlog.address.commons.core.LogsCenter;
@@ -26,7 +30,7 @@ public class RsvpPanel extends UiPart<Region> {
     private Label rsvpLabel;
 
     @FXML
-    private StackPane piechartPlaceholder;
+    private HBox piechartPlaceholder;
 
     /**
      * Creates a {@code StatisticsPanel}
@@ -41,14 +45,32 @@ public class RsvpPanel extends UiPart<Region> {
 
     private PieChart generatePiechart() {
         RsvpStatistics rsvpStatistics = logic.getRsvpStatistics();
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("No", rsvpStatistics.getPercentGuestsRsvpNo()),
-                        new PieChart.Data("Unknown", rsvpStatistics.getPercentGuestsRsvpUnknown()),
-                        new PieChart.Data("Yes", rsvpStatistics.getPercentGuestsRsvpYes()));
+        int rsvpNo = rsvpStatistics.getPercentGuestsRsvpNo();
+        int rsvpYes = rsvpStatistics.getPercentGuestsRsvpYes();
+        int rsvpUnknown = rsvpStatistics.getPercentGuestsRsvpUnknown();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        if (rsvpNo > 0) {
+            pieChartData.add(new PieChart.Data("No", rsvpNo));
+        }
+        if (rsvpYes > 0) {
+            pieChartData.add(new PieChart.Data("Yes", rsvpYes));
+        }
+        if (rsvpUnknown > 0) {
+            pieChartData.add(new PieChart.Data("Unknown", rsvpUnknown));
+        }
+//                FXCollections.observableArrayList(
+//                        new PieChart.Data("No", rsvpStatistics.getPercentGuestsRsvpNo()),
+//                        new PieChart.Data("Unknown", rsvpStatistics.getPercentGuestsRsvpUnknown()),
+//                        new PieChart.Data("Yes", rsvpStatistics.getPercentGuestsRsvpYes()));
+
+
 
         final PieChart chart = new PieChart(pieChartData);
+        chart.setLabelsVisible(true);
+        chart.setLabelLineLength(10);
         chart.setLegendVisible(true);
+        chart.setLegendSide(LEFT);
+        chart.setPadding(new Insets(0));
         return chart;
     }
 }
