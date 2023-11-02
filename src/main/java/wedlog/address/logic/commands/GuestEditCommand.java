74e63101f8cf9d208bb2228c9610d427197093ff
tllshan <105906799+tllshan@users.noreply.github.com>
2023-11-02 20:36:32 +0800

    Update src/main/java/wedlog/address/logic/commands/GuestEditCommand.java
    Co-authored-by: p-xp <99188356+p-xp@users.noreply.github.com>

--- src/main/java/wedlog/address/logic/commands/GuestEditCommand.java
+++ src/main/java/wedlog/address/logic/commands/GuestEditCommand.java
@@ -62,7 +62,7 @@ public class GuestEditCommand extends Command {
 
     public static final String MESSAGE_EDIT_GUEST_SUCCESS = "Edited guest: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_GUEST = "This guest already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_GUEST = "A guest with this name already exists in WedLog.";
 
     private final Index index;
     private final EditGuestDescriptor editGuestDescriptor;
