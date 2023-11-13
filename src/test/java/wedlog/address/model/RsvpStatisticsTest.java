5ea2c73021d56da6008fdbf05e868af63d86697a
tllshan <shantsou03@gmail.com>
2023-11-13 13:35:37 +0800

    Add tests for rsvp statistics

--- src/test/java/wedlog/address/model/RsvpStatisticsTest.java
+++ src/test/java/wedlog/address/model/RsvpStatisticsTest.java
@@ -2,6 +2,7 @@ package wedlog.address.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,24 @@ public class RsvpStatisticsTest {
     }
 
     @Test
+    public void testGetNumGuestsRsvpYes() {
+        int result = testRsvpStatistics.getNumGuestsRsvpYes();
+        assertEquals(50, result);
+    }
+
+    @Test
+    public void testGetNumGuestsRsvpNo() {
+        int result = testRsvpStatistics.getNumGuestsRsvpNo();
+        assertEquals(20, result);
+    }
+
+    @Test
+    public void testGetNumGuestsRsvpUnknown() {
+        int result = testRsvpStatistics.getNumGuestsRsvpUnknown();
+        assertEquals(30, result);
+    }
+
+    @Test
     public void testEquals_same() {
         assertEquals(new RsvpStatistics(50, 20, 30), new RsvpStatistics(50, 20, 30));
     }
@@ -22,5 +41,15 @@ public class RsvpStatisticsTest {
     @Test
     public void testEquals_notSame() {
         assertNotEquals(new RsvpStatistics(50, 20, 30), new RsvpStatistics(50, 30, 20));
+    }
+
+    @Test
+    public void equals_notRsvpStatistics_returnsFalse() {
+        assertNotEquals(testRsvpStatistics, 0);
+    }
+
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        assertTrue(testRsvpStatistics.equals(testRsvpStatistics));
     }
 }
